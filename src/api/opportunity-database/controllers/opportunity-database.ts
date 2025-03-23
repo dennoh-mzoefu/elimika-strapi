1f7710c43318e7a11301bfbfbb79f7b9f88d3925
dennoh-mzoefu <denisgichana162@gmail.com>
2025-03-23 23:25:57 +0300

    added content type: publications,events,oppportunity database

---
+++ src/api/opportunity-database/controllers/opportunity-database.ts
@@ -0,0 +1,7 @@
+/**
+ * opportunity-database controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::opportunity-database.opportunity-database');
---
+++ src/api/opportunity-database/routes/opportunity-database.ts
@@ -0,0 +1,7 @@
+/**
+ * opportunity-database router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::opportunity-database.opportunity-database');
